fb0e2705ca19a156caf7f885102f9eb1306f9399
Cris Lucero <lucerocris22@gmail.com>
2025-01-29 08:40:17 +0800

    Fixed Project Languages

--- Script/script.js
+++ Script/script.js
@@ -1,8 +1,10 @@
 //DOM
-const profileImage = document.getElementsByClassName('profile')[0]; 
+const profileImage = document.getElementsByClassName('profile')[0];
 const projectsContainer = document.querySelector('.projects-grid');
-const projContainer = document.querySelectorAll('.proj'); 
-
+const projContainer = document.querySelectorAll('.proj');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const messageInput = document.querySelector('#message');
 
 // Media queries
 const mediaTablet = window.matchMedia('(max-width: 1024px)');
@@ -22,16 +24,15 @@ function updateImageSrc() {
 // Function to change innerHTML on different devices
 
 function changeProjHTML() {
-  
   let projCount = 1;
   projContainer.forEach((project) => {
-   
     project.innerHTML = '';
 
     // Create the new content
     const div = document.createElement('div');
     if (mediaMobile.matches) {
-      div.innerHTML = `
+      if (projCount >= 3) {
+        div.innerHTML = `
         <div class="img-container">
           <img src="./assets/images/thumbnail-project-${projCount}-large.webp" alt="" />
         </div>
@@ -41,6 +42,7 @@ function changeProjHTML() {
           <div class="lang-used">
             <p>HTML</p>
             <p>CSS</p>
+            <p>JAVASCRIPT</p>
           </div>
           </div>
           <div class="btn-container">
@@ -48,8 +50,28 @@ function changeProjHTML() {
             <div class="btn-proj">VIEW CODE</div>
           </div>
         </div>`;
+      } else {
+        div.innerHTML = `
+          <div class="img-container">
+            <img src="./assets/images/thumbnail-project-${projCount}-large.webp" alt="" />
+          </div>
+          <div class="proj-info">
+          <div>
+            <h4>DESIGN PORTFOLIO</h4>
+            <div class="lang-used">
+              <p>HTML</p>
+              <p>CSS</p>
+            </div>
+            </div>
+            <div class="btn-container">
+              <div class="btn-proj">VIEW PROJECT</div>
+              <div class="btn-proj">VIEW CODE</div>
+            </div>
+          </div>`;
+      }
     } else if (mediaTablet.matches) {
-      div.innerHTML = `
+      if (projCount >= 3) {
+        div.innerHTML = `
         <div class="img-container">
           <img src="./assets/images/thumbnail-project-${projCount}-large.webp" alt="" />
         </div>
@@ -59,6 +81,7 @@ function changeProjHTML() {
           <div class="lang-used">
             <p>HTML</p>
             <p>CSS</p>
+            <p>JAVASCRIPT</p>
           </div>
           </div>
           <div class="btn-container">
@@ -66,8 +89,28 @@ function changeProjHTML() {
             <div class="btn-proj">VIEW CODE</div>
           </div>
         </div>`;
+      } else {
+        div.innerHTML = `
+          <div class="img-container">
+            <img src="./assets/images/thumbnail-project-${projCount}-large.webp" alt="" />
+          </div>
+          <div class="proj-info">
+          <div>
+            <h4>DESIGN PORTFOLIO</h4>
+            <div class="lang-used">
+              <p>HTML</p>
+              <p>CSS</p>
+            </div>
+            </div>
+            <div class="btn-container">
+              <div class="btn-proj">VIEW PROJECT</div>
+              <div class="btn-proj">VIEW CODE</div>
+            </div>
+          </div>`;
+      }
     } else {
-      div.innerHTML = `
+      if (projCount >= 3) {
+        div.innerHTML = `
         <div class="img-container">
           <img src="./assets/images/thumbnail-project-${projCount}-large.webp" alt="" />
           <div class="btn-container">
@@ -80,20 +123,35 @@ function changeProjHTML() {
           <div class="lang-used">
             <p>HTML</p>
             <p>CSS</p>
+            <p>JAVASCRIPT</p>
           </div>
         </div>`;
+      } else {
+        div.innerHTML = `
+          <div class="img-container">
+            <img src="./assets/images/thumbnail-project-${projCount}-large.webp" alt="" />
+            <div class="btn-container">
+              <div class="btn-proj">VIEW PROJECT</div>
+              <div class="btn-proj">VIEW CODE</div>
+            </div>
+          </div>
+          <div class="proj-info">
+            <h4>DESIGN PORTFOLIO</h4>
+            <div class="lang-used">
+              <p>HTML</p>
+              <p>CSS</p>
+            </div>
+          </div>`;
+      }
     }
 
-    
     project.appendChild(div);
     projCount++;
   });
 }
 
-
 if (profileImage) updateImageSrc();
 changeProjHTML();
-
 
 mediaTablet.addEventListener('change', () => {
   if (profileImage) updateImageSrc();
